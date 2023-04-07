26e5328119873d5f48f9dac285771a994878aa8a
stehanget <61905668+stehanget@users.noreply.github.com>
2023-04-08 05:23:20 +0700

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server"
 
 export function middleware(request) {
-    console.log(request.nextUrl.pathname)
     if (request.nextUrl.pathname === "/clientzone") {
         return NextResponse.redirect(new URL("/clientzone/dashboard", request.url))
     }
