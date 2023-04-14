07e16f9b82791ce9ee2fc6dd5727d87d63639ca4
nesyahcasdfgjkl <102537045+nesyahcasdfgjkl@users.noreply.github.com>
2023-04-14 19:02:04 +0700

    add routing

--- app/(public)/layout.js
+++ app/(public)/layout.js
@@ -1,56 +1,137 @@
-"use client"
-import '../globals.css'
-import Fonts from '@/constants/fonts'
-import { RecoilRoot } from 'recoil'
-import { Montserrat } from '@next/font/google'
-import Link from 'next/link'
-import Image from 'next/image'
+"use client";
+import "../globals.css";
+import Fonts from "@/constants/fonts";
+import { RecoilRoot } from "recoil";
+import { Montserrat } from "@next/font/google";
+import Link from "next/link";
+import Image from "next/image";
+import { Menu, Transition } from "@headlessui/react";
+import { ChevronDoubleDownIcon } from "@heroicons/react/20/solid";
+import { Fragment } from "react";
 const montserrat = Montserrat({
-  subsets: ['latin'],
-  variable: '--font-montserrat',
+  subsets: ["latin"],
+  variable: "--font-montserrat",
   display: "fallback", // <--
-})
+});
 
 export default function RootLayout({ children }) {
-
   return (
     <html lang="en">
       <head />
-      <body className={[Fonts.className, "font-montserrat"].join(' ')}>
-        <nav className={`${montserrat.variable} font-sans py-6 px-16 shadow-md flex items-center bg-white text-white`}>
+      <body className={[Fonts.className, "font-montserrat"].join(" ")}>
+        <nav
+          className={`${montserrat.variable} font-sans py-6 px-16 shadow-md flex items-center bg-white text-white`}
+        >
           <Link href="/">
             <Image
               src="/logo.svg"
               width={1200}
               height={1200}
-
               className="w-32 object-contain self-center mr-8"
             />
           </Link>
-          <div className='flex-1 flex-row space-x-12'>
-            <Link href={"/project "} className="text-base font-medium text-[#6E6F70]">Home</Link>
-            <Link href={"/project"} className="text-base font-medium text-[#6E6F70]">Pricing</Link>
-            <Link href={"/project"} className="text-base font-medium text-[#6E6F70]">About</Link>
+          <div className="flex-1 flex-row space-x-12">
+            <Link
+              href={"/project "}
+              className="text-base font-medium text-[#6E6F70]"
+            >
+              Home
+            </Link>
+            <Link
+              href={"/project"}
+              className="text-base font-medium text-[#6E6F70]"
+            >
+              Pricing
+            </Link>
+            <Link
+              href={"/project"}
+              className="text-base font-medium text-[#6E6F70]"
+            >
+              About
+            </Link>
           </div>
-          <div className='flex flex-row space-x-3'>
-            <Image
-              className='rounded-full w-10 h-10'
-              src="https://picsum.photos/id/237/400/400"
-              alt="Picture of the author"
-              width={32}
-
-              height={32}
-            />
-            <button className='bg-secondary text-white px-4 py-2 text-sm font-medium rounded-md '>Logout</button>
+          <div className="flex flex-row space-x-3">
+            <Menu as="div" className="relative inline-block text-left">
+              <div>
+                <Menu.Button className="inline-flex w-full justify-center items-center rounded-md bg-secondary px-2 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+                  <Image
+                    className="rounded-full w-6 h-6 mx-2"
+                    src="https://picsum.photos/id/237/400/400"
+                    alt="Picture of the author"
+                    width={32}
+                    height={32}
+                  />
+                  Wildan Romiza
+                  <ChevronDoubleDownIcon
+                    className="ml-2 -mr-1 h-5 w-5 text-white hover:text-violet-100"
+                    aria-hidden="true"
+                  />
+                </Menu.Button>
+              </div>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <div className="px-1 py-1">
+                    <Menu.Item>
+                      {({ active }) => (
+                        <button
+                          className={`${
+                            active
+                              ? "bg-violet-500 text-white"
+                              : "text-gray-900"
+                          } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                        >
+                          Clientzone
+                        </button>
+                      )}
+                    </Menu.Item>
+                  </div>
+                  <div className="px-1 py-1">
+                    <Menu.Item>
+                      {({ active }) => (
+                        <button
+                          className={`${
+                            active
+                              ? "bg-violet-500 text-white"
+                              : "text-gray-900"
+                          } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                        >
+                          Profil
+                        </button>
+                      )}
+                    </Menu.Item>
+                  </div>
+                  <div className="px-1 py-1">
+                    <Menu.Item>
+                      {({ active }) => (
+                        <button
+                          className={`${
+                            active
+                              ? "bg-violet-500 text-white"
+                              : "text-gray-900"
+                          } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                        >
+                          Logout
+                        </button>
+                      )}
+                    </Menu.Item>
+                  </div>
+                </Menu.Items>
+              </Transition>
+            </Menu>
           </div>
         </nav>
-        <div >
-          <RecoilRoot>
-            {children}
-          </RecoilRoot>
+        <div>
+          <RecoilRoot>{children}</RecoilRoot>
         </div>
-
       </body>
     </html>
-  )
+  );
 }
--- components/project/index.js
+++ components/project/index.js
@@ -1,38 +1,46 @@
-import Image from 'next/image'
-import React from 'react'
-import { AiFillStar } from 'react-icons/ai'
-import { FaCommentAlt } from 'react-icons/fa'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { AiFillStar } from "react-icons/ai";
+import { FaCommentAlt } from "react-icons/fa";
 
 export default function Project() {
-    return (
-        <div className='flex flex-col'>
+  return (
+    <div className="flex flex-col">
+      <Link href={"/project/2"}>
+        <Image
+          src={
+            "https://lh5.googleusercontent.com/8gwdZIlrONenDA5NJsi6tLsSFXOijfkzeOOVTEG8P1u2lPntRLKdgL6ozNkldkvb8Fcf801B6s2pj2_dNtoaVTTKDlHSZ6ViSwgZZPi8JtlbUUKGiiyvInV2IbC_49BjB1xgcKgc"
+          }
+          width={1200}
+          height={1200}
+          className="rounded-lg w-fit h-fit object-cover mt-8"
+        />
+        <div className="flex flex-row mt-2 items-center">
+          <p className="font-semibold">Portofolio Website with elegan color </p>
+        </div>
+      </Link>
+      <div className="flex flex-row mt-2 items-center">
+        <div className="basis-2/3  flex flex-row items-center">
+          <Link href={"/project"} className="flex flex-row items-center">
             <Image
-                src={"https://lh5.googleusercontent.com/8gwdZIlrONenDA5NJsi6tLsSFXOijfkzeOOVTEG8P1u2lPntRLKdgL6ozNkldkvb8Fcf801B6s2pj2_dNtoaVTTKDlHSZ6ViSwgZZPi8JtlbUUKGiiyvInV2IbC_49BjB1xgcKgc"}
-                width={1200}
-                height={1200}
-                className="rounded-lg w-fit h-fit object-cover mt-8" />
-            <div className='flex flex-row mt-2 items-center'>
-                <p className='font-semibold'>Portofolio Website with elegan color </p>
-            </div>
-            <div className='flex flex-row mt-2 items-center'>
-                <div className="basis-2/3  flex flex-row items-center">
-                    <Image
-                        className='rounded-full w-6 h-6'
-                        src="https://picsum.photos/id/237/400/400"
-                        alt="Picture of the author"
-                        width={16}
-
-                        height={16}
-                    />
-                    <p className='text-black pl-3 text-sm'>Wildan Romiza</p></div>
-                <div className="basis-1/3 justify-end flex flex-row items-center mr-2">
-                    <AiFillStar width={8} height={8} />
-                    <p className="mr-2">6</p>
-                    <FaCommentAlt width={4} height={4} className="mr-1" />
-                    <p>6</p>
-                </div>
-            </div>
-        </div >
-
-    )
+              src={
+                "https://st3.depositphotos.com/1017228/18878/i/950/depositphotos_188781580-stock-photo-handsome-cheerful-young-man-standing.jpg"
+              }
+              width={1200}
+              height={1200}
+              className="rounded-full w-10 h-10 object-cover "
+            />
+            <p className="text-black pl-3 text-sm">Wildan Romiza</p>
+          </Link>
+        </div>
+        <div className="basis-1/3 justify-end flex flex-row items-center mr-2">
+          <AiFillStar width={8} height={8} />
+          <p className="mr-2">6</p>
+          <FaCommentAlt width={4} height={4} className="mr-1" />
+          <p>6</p>
+        </div>
+      </div>
+    </div>
+  );
 }
