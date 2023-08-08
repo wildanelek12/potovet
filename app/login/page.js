f3fd6f01e1728a6dc4f8e7add5bf26d6d5940095
wildan <wildan.romiza@gmail.com>
2023-08-08 08:54:04 +0700

    sign-up page

--- app/login/page.js
+++ app/login/page.js
@@ -106,7 +106,7 @@ export default function Page() {
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Don’t have an account yet?{" "}
                 <Link
-                  href="/login"
+                  href="/sign-up"
                   className="font-medium text-primary-600 hover:underline dark:text-primary-500"
                 >
                   Sign Up
