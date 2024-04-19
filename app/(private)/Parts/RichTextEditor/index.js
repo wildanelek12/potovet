63944839425b9d94cf3a4eacc682fee1df91d7c2
wildan <wildan.romiza@gmail.com>
2024-04-19 10:54:28 +0700

    fix some bug

--- app/(private)/Parts/RichTextEditor/index.js
+++ app/(private)/Parts/RichTextEditor/index.js
@@ -7,39 +7,10 @@ import { FiRefreshCcw } from "react-icons/fi";
 import { Dialog, Transition } from "@headlessui/react";
 import { template } from "lodash";
 const editorConfiguration = {
-  toolbar: [
-    "heading",
-    "|",
-    "bold",
-    "italic",
-    "|",
-    "link",
-    "|",
-    "outdent",
-    "indent",
-    "|",
-    "bulletedList",
-    "numberedList",
-    "|",
-    "insertTable",
-    "|",
-    "uploadImage",
-    "blockQuote",
-    "|",
-    "undo",
-    "redo",
-  ],
+  toolbar: ["heading", "|", "bold", "italic", "|", "link", "|", "outdent", "indent", "|", "bulletedList", "numberedList", "|", "insertTable", "|", "uploadImage", "blockQuote", "|", "undo", "redo"],
 };
 
-export default function RichTextEditor({
-  value,
-  onChange,
-  label,
-  isTextArea,
-  hintText,
-  template,
-  isHaveTemplate = true,
-}) {
+export default function RichTextEditor({ value, onChange, label, isTextArea, hintText, template, isHaveTemplate = true }) {
   const editorRef = useRef();
   const [rendered, setRendered] = useState(false);
   const [count, setCount] = useState(0);
@@ -80,32 +51,18 @@ export default function RichTextEditor({
   }
 
   return (
-    <div className="grid mt-4">
+    <div className="grid mt-4 ">
       <div className="flex items-center ">
         {label && <label className="font-semibold capitalize">{label}</label>}
 
-        <button
-          type="button"
-          className="relative z-50 flex items-center px-3 py-1 ml-2 text-sm font-bold text-white bg-transparent rounded-full group"
-          onClick={openModal}
-        >
+        <button type="button" className="relative z-10 flex items-center px-3 py-1 ml-2 text-sm font-bold text-white bg-transparent rounded-full group" onClick={openModal}>
           <AiFillQuestionCircle className="" color="blue" size={24} />
-          <span className="absolute bottom-0 p-2 text-xs text-white scale-0 bg-gray-800 rounded left-10 group-hover:scale-100 whitespace-nowrap">
-            Get Info
-          </span>
+          <span className="absolute bottom-0 p-2 text-xs text-white scale-0 bg-gray-800 rounded left-10 group-hover:scale-100 whitespace-nowrap">Get Info</span>
         </button>
 
         <Transition appear show={isOpen} as={Fragment}>
-          <Dialog as="div" className="relative z-50" onClose={closeModal}>
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
+          <Dialog as="div" className="relative z-10" onClose={closeModal}>
+            <Transition.Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0" enterTo="opacity-100" leave="ease-in duration-200" leaveFrom="opacity-100" leaveTo="opacity-0">
               <div className="fixed inset-0 bg-black/25" />
             </Transition.Child>
 
@@ -121,10 +78,7 @@ export default function RichTextEditor({
                   leaveTo="opacity-0 scale-95"
                 >
                   <Dialog.Panel className="w-full max-w-md p-6 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
-                    <Dialog.Title
-                      as="h3"
-                      className="text-lg font-medium leading-6 text-gray-900"
-                    >
+                    <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
                       Information
                     </Dialog.Title>
                     <div className="mt-2">
@@ -149,45 +103,24 @@ export default function RichTextEditor({
 
         {isHaveTemplate && (
           <>
-            <button
-              type="button"
-              className="flex items-center px-3 py-1 ml-auto text-sm font-bold text-white rounded-md bg-secondary"
-              onClick={insertTemplate}
-            >
+            <button type="button" className="flex items-center px-3 py-1 ml-auto text-sm font-bold text-white rounded-md bg-secondary" onClick={insertTemplate}>
               <RiStackFill className="mr-2" /> {/* Icon component */}
               Template
             </button>
 
-            <button
-              type="button"
-              className="relative z-50 flex items-center px-3 py-1 ml-2 text-sm font-bold text-white bg-blue-500 rounded-md group"
-              onClick={insertTemplate}
-            >
+            <button type="button" className="relative z-10 flex items-center px-3 py-1 ml-2 text-sm font-bold text-white bg-blue-500 rounded-md group" onClick={insertTemplate}>
               <FiRefreshCcw className="" />
-              <span className="absolute p-2 text-xs text-white scale-0 bg-gray-800 rounded bottom-10 group-hover:scale-100">
-                Reset
-              </span>
+              <span className="absolute p-2 text-xs text-white scale-0 bg-gray-800 rounded bottom-10 group-hover:scale-100">Reset</span>
             </button>
           </>
         )}
       </div>
 
-      <div className="py-1 mt-2 h-fit">
-        {rendered && (
-          <CKEditor
-            editor={Editor}
-            data={value}
-            config={!isTextArea && editorConfiguration}
-            onChange={(_, editor) => onChange(handleChange(editor))}
-          />
-        )}
+      <div className="py-1 mt-2 h-fit ">
+        {rendered && <CKEditor editor={Editor} data={value} config={!isTextArea && editorConfiguration} onChange={(_, editor) => onChange(handleChange(editor))} />}
       </div>
 
-      {!value ? (
-        <p className="text-sm font-normal text-red-500">
-          Isikan Kolom dengan kombinasi huruf, video, dan angka.
-        </p>
-      ) : null}
+      {!value ? <p className="text-sm font-normal text-red-500">Isikan Kolom dengan kombinasi huruf, video, dan angka.</p> : null}
     </div>
   );
 }
--- app/(private)/Parts/Select/index.js
+++ app/(private)/Parts/Select/index.js
@@ -1,53 +1,37 @@
-import { Listbox, Transition } from '@headlessui/react'
-import { Fragment } from 'react'
-import { ChevronUpDownIcon } from '@heroicons/react/20/solid'
-import { cn } from '@/utils'
+import { Listbox, Transition } from "@headlessui/react";
+import { Fragment } from "react";
+import { ChevronUpDownIcon } from "@heroicons/react/20/solid";
+import { cn } from "@/utils";
 
 export default function Select({ labelClassname, label, onChange, options, value, defaultValue, selected }) {
-	return (
-		<div className="flex flex-col w-full gap-1">
-			{label && <label className={cn('capitalize font-semibold', labelClassname)}>{label}</label>}
+  return (
+    <div className="flex flex-col w-full gap-1">
+      {label && <label className={cn("capitalize font-semibold", labelClassname)}>{label}</label>}
 
-			<Listbox value={value ?? defaultValue} onChange={(e) => onChange(e)}>
-				<div className="relative">
-					<Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white border border-[#CCCED1] rounded-sm cursor-pointer">
-						<span className="block text-base truncate">{selected.name ?? 'Select Categories'}</span>
-						<span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-							<ChevronUpDownIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
-						</span>
-					</Listbox.Button>
+      <Listbox value={value ?? defaultValue} onChange={(e) => onChange(e)}>
+        <div className="relative">
+          <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white border border-[#CCCED1] rounded-sm cursor-pointer">
+            <span className="block text-base truncate">{selected.name ?? "Select Categories"}</span>
+            <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+              <ChevronUpDownIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
+            </span>
+          </Listbox.Button>
 
-					<Transition
-						as={Fragment}
-						leave="transition ease-in duration-100"
-						leaveFrom="opacity-100"
-						leaveTo="opacity-0"
-					>
-						<Listbox.Options className="absolute z-10 w-full py-1 mt-1 bg-white rounded-sm shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 !outline-none">
-							{options.map((option, optionIdx) => (
-								<Listbox.Option
-									key={optionIdx}
-									className={({ active }) =>
-										cn(
-											'relative cursor-default select-none py-2 pl-2 pr-8',
-											active ? 'bg-white text-amber-900' : 'text-gray-900'
-										)
-									}
-									value={option}
-								>
-									{({ selected }) => (
-										<span
-											className={cn('block truncate', selected ? 'font-medium' : 'font-normal')}
-										>
-											{option.name}
-										</span>
-									)}
-								</Listbox.Option>
-							))}
-						</Listbox.Options>
-					</Transition>
-				</div>
-			</Listbox>
-		</div>
-	)
+          <Transition as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
+            <Listbox.Options className="absolute z-50 w-full py-1 mt-1 bg-white rounded-sm shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 !outline-none">
+              {options.map((option, optionIdx) => (
+                <Listbox.Option
+                  key={optionIdx}
+                  className={({ active }) => cn("relative cursor-default select-none py-2 pl-2 pr-8", active ? "bg-white text-amber-900" : "text-gray-900")}
+                  value={option}
+                >
+                  {({ selected }) => <span className={cn("block truncate", selected ? "font-medium" : "font-normal")}>{option.name}</span>}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
+    </div>
+  );
 }
