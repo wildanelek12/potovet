20f03f15d12fdd366d906e58a0febd6deca9d519
wildan romiza <38659848+wildanelek12@users.noreply.github.com>
2024-12-05 09:30:20 +0700

    Update index.js

--- utils/Axios/index.js
+++ utils/Axios/index.js
@@ -1,51 +1,55 @@
 /** @format */
 
-import axios from "axios"
-import { Toast, ToastLoading } from "../SweetAlert"
+import axios from "axios";
+import { Toast, ToastLoading } from "../SweetAlert";
 
 export const axiosBaseQuery =
-	({ baseUrl } = { baseUrl: "" }) =>
-	async ({ url, method, data, params, headers }) => {
-		const token = localStorage.getItem("token")
+  ({ baseUrl } = { baseUrl: "" }) =>
+  async ({ url, method, data, params, headers }) => {
+    const token = localStorage.getItem("token");
 
-		try {
-			const isVisible = Toast.isVisible()
-			if (!isVisible) {
-				ToastLoading.fire({
-					title: "Loading...",
-					didOpen: () => {
-						ToastLoading.showLoading()
-					},
-				})
-			}
+    try {
+      const isVisible = Toast.isVisible();
+      if (!isVisible) {
+        ToastLoading.fire({
+          title: "Loading...",
+          didOpen: () => {
+            ToastLoading.showLoading();
+          },
+        });
+      }
 
-			const res = await axios({
-				url: baseUrl + url,
-				method,
-				data,
-				params,
-				headers: {
-					Authorization: token && "Bearer " + token,
-					...headers,
-				},
-			})
+      const res = await axios({
+        url: baseUrl + url,
+        method,
+        data,
+        params,
+        headers: {
+          Authorization: token && "Bearer " + token,
+          ...headers,
+        },
+      });
 
-			if (!isVisible) {
-				ToastLoading.close()
-			}
+      if (!isVisible) {
+        ToastLoading.close();
+      }
 
-			return { data: res.data }
-		} catch (err) {
-			Toast.fire({
-				icon: "error",
-				title: err?.response.data.message,
-			})
+      return { data: res.data };
+    } catch (err) {
+      if (err.response?.status === 401) {
+        window.location.href = "/login";
+      } else {
+        Toast.fire({
+          icon: "error",
+          title: err?.response.data.message,
+        });
 
-			return {
-				error: {
-					status: err.response?.status,
-					data: err.response?.data || err.message,
-				},
-			}
-		}
-	}
+        return {
+          error: {
+            status: err.response?.status,
+            data: err.response?.data || err.message,
+          },
+        };
+      }
+    }
+  };
